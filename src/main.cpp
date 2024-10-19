84fcf4df783ce44277352f0aedc66649526b3c4c
Matias Piispanen <Matias.Piispanen@gmail.com>
2024-10-19 23:30:54 +0200

    Add cxxopts dependency and replace getopt with cxxopts for argument parsing
    * **CMakeLists.txt**
      - Add cxxopts as a dependency
      - Include cxxopts header file
    
    * **src/main.cpp**
      - Replace getopt with cxxopts for argument parsing
      - Add cxxopts.hpp include
      - Update argument parsing logic to use cxxopts
    
    * **.github/workflows/ci.yml**
      - Set SDL2_DIR environment variable before Configure CMake step
    
    * **external/cxxopts**
      - Add cxxopts as a submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <string>
-
-#ifdef _WIN32
-#include <windows.h>
-#include "getopt.h" // Ensure you have a getopt implementation for Windows
-#else
-#include <unistd.h>
-#endif
+#include <cxxopts.hpp>
 
 int main(int argc, char *argv[])
 {
@@ -16,25 +10,28 @@ int main(int argc, char *argv[])
     int windowHeight = 600;
     bool fullscreen = false;
 
-    // Parse command-line arguments
-    int opt;
-    while ((opt = getopt(argc, argv, "w:h:f")) != -1)
+    // Parse command-line arguments using cxxopts
+    try
     {
-        switch (opt)
+        cxxopts::Options options(argv[0], "Platformer Prototype");
+        options.add_options()
+            ("w,width", "Window width", cxxopts::value<int>(windowWidth)->default_value("800"))
+            ("h,height", "Window height", cxxopts::value<int>(windowHeight)->default_value("600"))
+            ("f,fullscreen", "Fullscreen mode", cxxopts::value<bool>(fullscreen)->default_value("false"))
+            ("help", "Print help");
+
+        auto result = options.parse(argc, argv);
+
+        if (result.count("help"))
         {
-        case 'w':
-            windowWidth = std::stoi(optarg);
-            break;
-        case 'h':
-            windowHeight = std::stoi(optarg);
-            break;
-        case 'f':
-            fullscreen = true;
-            break;
-        default:
-            std::cerr << "Usage: " << argv[0] << " [-w width] [-h height] [-f fullscreen]" << std::endl;
-            return 1;
+            std::cout << options.help() << std::endl;
+            return 0;
         }
+    }
+    catch (const cxxopts::OptionException &e)
+    {
+        std::cerr << "Error parsing options: " << e.what() << std::endl;
+        return 1;
     }
 
     // Initialize SDL2
